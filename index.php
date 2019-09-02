9c7fff99a5d56cbacde31bfb07fa987eee3a5a0b
Itrio Netuno <itrionet@gmail.com>
2019-09-02 19:43:27 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<link rel="stylesheet" href="vendor/materialize/css/materialize.min.css">
+</head>
+<body>
+
+<nav>
+    <div class="nav-wrapper blue">
+        <a href="#" class="brand-logo center">Pingador</a>
+    </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+        <form class="col s12">
+            <div class="row">
+                <div class="col s12">
+                    <label for="textarea1">Insira os IPs que deseja pingar. Coloque um IP em cada linha.<br></label>
+                    <textarea id="textarea1" rows="15"></textarea>
+                </div>
+                <div class="col s12">
+                    <a class="waves-effect waves-light btn blue" style="width: 100%; margin-top: 10px;">PINGAR!</a>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<div class="divider"></div>
+
+
+
+</body>
+
+<script type="text/javascript" src="vendor/jquery/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="vendor/materialize/js/materialize.min.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        //
+    });
+</script>
+
+</html>
